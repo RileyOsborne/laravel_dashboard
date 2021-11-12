9981498e0e9ddd2b39f78a0660c39b7d94d30386
Riley Osborne <riley@fineprintdata.com>
2021-11-12 14:39:15 -0500

    AddingBaseCode

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+</head>
+
+<body>
+  
+  <div>
+    <p>Companies</p>
+  </div>
+  <div>
+    <p><button>Create new company</button></p>
+  </div>
+  <div>
+    <p>Compaies list</p>
+  </div>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Address</th>
+      <th>Website</th>
+      <th>Email</th>
+      <th>Actions</th>
+    </tr>
+    <tr>
+      <td>Riley Osborne</td>
+      <td>107 Padgett St. Corbin, Ky 40701</td>
+      <td><a href="https://google.com">www.google.com</td>
+    </tr>
+  </table>
+</body>
